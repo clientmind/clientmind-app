fadf350ee3e2d2e4dd8ad36a41752ad9e2ba70e6
Jim Phieffer <jameslp7324@gmail.com>
2025-01-07 16:45:46 -0500

    chore(app): updated commitlint to require scope

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,10 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		// Enforce a scope is always required
+		'scope-empty': [2, 'never'],
+
+		// Restrict scopes to the allowed list
+		'scope-enum': [2, 'always', ['app', 'api', 'db']]
+	}
+};
